f843789e759996ee6400e21a8dbb8b56488c8109
Haadia Batool <haadiabatool971@gmail.com>
2026-08-21 09:50:45 +0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+
+// When click on do-darya do-darya site open
+const pagedodarya=document.getElementById('page-do-darya')
+
+pagedodarya.addEventListener("click",()=>{
+    window.location.href="do-darya.html";
+});
+
+// When click on sukkur , sukkur site open
+const pagesukkur=document.getElementById('page-sukkur')
+
+pagesukkur.addEventListener("click",()=>{
+    window.location.href="sukkur.html";
+});
