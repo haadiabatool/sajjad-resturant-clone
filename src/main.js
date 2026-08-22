cf259c823a8fe1d28bfb56c30517b88dbb6869d7
Haadia Batool <haadiabatool971@gmail.com>
2026-08-22 15:32:29 +0500

    update

--- src/main.js
+++ src/main.js
@@ -1,14 +1,35 @@
+window.onload = function() {
 
 // When click on do-darya do-darya site open
-const pagedodarya=document.getElementById('page-do-darya')
+const pagedodarya=
+    document.getElementById('page-do-darya')
 
 pagedodarya.addEventListener("click",()=>{
     window.location.href="do-darya.html";
 });
 
 // When click on sukkur , sukkur site open
-const pagesukkur=document.getElementById('page-sukkur')
+const pagesukkur=
+    document.getElementById('page-sukkur')
 
 pagesukkur.addEventListener("click",()=>{
     window.location.href="sukkur.html";
 });
+
+// Change color of Navbar during scroll
+
+const navbar=
+    document.getElementById('navbar')
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 50){
+        navbar.classList.remove("bg-transparent");
+        navbar.classList.add("bg-black");
+    }else{
+        navbar.classList.remove("bg-black");
+        navbar.classList.add("bg-transparent");
+    }
+    
+});
+
+};
